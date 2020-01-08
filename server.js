c36ef70e4e9048f414153638dbcb5b9958926b41
priyanka5b4 <sriramakavacham.priyanka@gmail.com>
2020-01-08 10:46:08 +0530

    first checkin

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port = procee.env.PORT || 3000
+app.use(express.static(__dirname + '/dist/ngproject'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
